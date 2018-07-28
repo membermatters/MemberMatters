a133033a1e5797622d258be8dedffb1969b71296
jabelone <hello@jaimyn.com.au>
2018-07-28 16:04:58 +1000

    Merged @nog3's report a spacebug feature

--- memberportal/portal/views.py
+++ memberportal/portal/views.py
@@ -1,8 +1,14 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.urls import reverse
 from memberportal.helpers import log_user_event
 from .forms import SpacebugForm
 import pytz
+import os
+import sendgrid
+from sendgrid.helpers.mail import *
+
 
 utc = pytz.UTC
 
@@ -38,7 +44,29 @@ def home(request):
 @login_required
 def spacebug(request):
     form_class = SpacebugForm
+    # if this is a submission, handle it and render a thankyou.
+    if request.method == 'POST':
+        form = form_class(data=request.POST)
+        if form.is_valid():
+            subject = request.POST.get('issue', '')
+            body = request.POST.get('details', '')
+            if "SENDGRID_API_KEY" in os.environ:
+                sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+                from_email = Email("portal@hsbne.org")
+                to_email = Email("issues@hsbne.org")
+                content = Content("text/html", body)
+                mail = Mail(from_email, subject, to_email, content)
+                response = sg.client.mail.send.post(request_body=mail.get())
+                if response.status_code == 202:
+                    log_user_event(request.user, "Sent issue with subject: " + subject, "email",
+                                   "Email content: " + body)
+                    messages.success(request, 'Issue submitted successfully. Thanks.')
+                    return redirect(reverse('report_spacebug'))
+        log_user_event(request.user, "Failed to send issue with subject: " + subject, "email", "Email content: " + body)
+        messages.error(request, 'Issue failed to submit, sorry.')
+        return redirect(reverse('report_spacebug'))
 
+    # render template normally
     return render(request, 'spacebug.html', {
         'form': form_class,
     })
