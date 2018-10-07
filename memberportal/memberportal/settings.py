d851f3b679baf817c55d8c25f7d3ed334876aa88
jabelone <hello@jaimyn.com.au>
2018-10-07 12:27:43 +1000

    Updated webcam snapshot links

--- memberportal/memberportal/settings.py
+++ memberportal/memberportal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l)#t68rzepzp)0l#x=9mntciapun$whl+$j&=_@nl^zl1xm3j*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["members.hsbne.org", "localhost", "127.0.0.1", "*"]
+ALLOWED_HOSTS = ["portal.hsbne.org", "porthack.hsbne.org", "localhost", "127.0.0.1"]
 
 
 # Application definition
