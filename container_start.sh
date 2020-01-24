52df59a40e641676cda4cc8ffae4116714dc60c3
Jaimyn Mayer <hello@jaimyn.com.au>
2020-01-25 00:30:31 +1000

    added auto migrate on container start

--- container_start.sh
+++ container_start.sh
@@ -4,4 +4,5 @@ service nginx start
 
 # Navigate to the app and start gunicorn
 cd memberportal
+python manage.py migrate
 exec gunicorn memberportal.wsgi:application --bind unix:/tmp/gunicorn.sock --access-logfile '/usr/src/logs/access.log' --error-logfile '/usr/src/logs/error.log' --workers 6
