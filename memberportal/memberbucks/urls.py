39a665e5230a331a42b17969959a5561bf29fffe
Jaimyn Mayer <hello@jaimyn.com.au>
2020-02-04 16:24:34 +1000

    maybe temp fix for vending machines

--- memberportal/memberbucks/urls.py
+++ memberportal/memberbucks/urls.py
@@ -11,9 +11,13 @@ urlpatterns = [
          name="delete_memberbucks_payment_info"),
     path("api/memberbucks/debit/", views.memberbucks_debit, name="memberbucks_debit"),
     path("api/memberbucks/debit/<int:rfid>/<int:amount>/", views.memberbucks_debit, name="memberbucks_debit"),
+    path("api/spacebucks/debit/<int:rfid>/<int:amount>/", views.memberbucks_debit, name="memberbucks_debit"),
     path("api/memberbucks/balance/<int:rfid>/", views.memberbucks_balance, name="memberbucks_balance"),
+    path("api/spacebucks/balance/<int:rfid>/", views.memberbucks_balance, name="memberbucks_balance"),
     path("api/memberbucks/groupdonation/<int:rfid>/<int:group_id>/<int:amount>/", views.memberbucks_group_donation,
          name="memberbucks_group_donation"),
     path("api/memberbucks/debit/<int:rfid>/<int:amount>/<str:description>", views.memberbucks_debit,
+         name="memberbucks_debit"),
+    path("api/spacebucks/debit/<int:rfid>/<int:amount>/<str:description>", views.memberbucks_debit,
          name="memberbucks_debit"),
 ]
