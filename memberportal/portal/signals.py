c302b2c6e7554863899abf13a78409afcf20098b
Jaimyn Mayer <hello@jaimyn.com.au>
2020-04-25 10:55:03 +1000

    removed unneeded static files

--- memberportal/portal/signals.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.signals import user_logged_in
-from django.dispatch import receiver
-from django.utils import timezone
-
-@receiver(user_logged_in)
-def sig_user_logged_in(sender, user, request, **kwargs):
-    user.profile.last_seen = timezone.now()
-    user.profile.save()
