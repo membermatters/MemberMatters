d43219aae2b87b8c0c18d8a4c3cc3709e20fff85
Jaimyn Mayer <hello@jaimyn.com.au>
2020-06-09 15:47:36 +1000

    added back missing email templates

--- memberportal/group/urls.py
+++ memberportal/group/urls.py
@@ -2,9 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("groups/", views.manage_groups, name="manage_groups"),
     path("group/<int:group_id>/delete/", views.delete_group, name="delete_group"),
-    path("group/<int:group_id>/edit/", views.edit_group, name="edit_group"),
-    path("group/<int:group_id>/email", views.email_group_members, name="email_group_members"),
-    path("group/list/", views.list_groups, name="list_groups"),
 ]
