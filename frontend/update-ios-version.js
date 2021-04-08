cac9d51a299a26f57aa6a0731ea0656a4194eea7
Jaimyn Mayer <github@jaimyn.com.au>
2021-04-08 12:13:36 +1000

    added update ios version script

---
+++ frontend/update-ios-version.js
@@ -0,0 +1,35 @@
+const xcode = require("xcode");
+const fs = require("fs");
+const pkg = require("./package.json");
+
+const projectPath = "src-capacitor/ios/App/App.xcodeproj/project.pbxproj";
+const project = xcode.project(projectPath);
+
+project.parse(() => {
+  const currentProjectVersion = project.getBuildProperty(
+    "CURRENT_PROJECT_VERSION",
+    "Release",
+    "App",
+  );
+
+  const newProjectVersion = currentProjectVersion + 1;
+  const newMarketingVersion = pkg.version;
+
+  for (const build of ["Debug", "Release"]) {
+    project.updateBuildProperty(
+      "CURRENT_PROJECT_VERSION",
+      newProjectVersion,
+      build,
+      "App",
+    );
+
+    project.updateBuildProperty(
+      "MARKETING_VERSION",
+      newMarketingVersion,
+      build,
+      "App",
+    );
+  }
+
+  fs.writeFileSync(projectPath, project.writeSync());
+});
