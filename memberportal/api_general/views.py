ff5f832ca340b49a32ce942e11dc0a29b51f7863
Jaimyn Mayer <github@jaimyn.com.au>
2021-06-22 20:16:51 +1000

    fixed induction email bug

--- memberportal/api_general/views.py
+++ memberportal/api_general/views.py
@@ -631,6 +631,20 @@ class Register(APIView):
 
         profile.email_profile_to(config.EMAIL_ADMIN)
 
+        if not config.ENABLE_STRIPE_MEMBERSHIP_PAYMENTS:
+            new_user.email_link(
+                f"Action Required: {config.SITE_OWNER} New Member Signup",
+                "Next Step: Register for an Induction",
+                "Important. Please read this email for details on how to "
+                "register for an induction.",
+                f"Hi {profile.first_name}, thanks for signing up! The next step to becoming a fully "
+                "fledged member is to book in for an induction. During this "
+                "induction we will go over the basic safety and operational "
+                f"aspects of {config.SITE_OWNER}. To book in, click the link below.",
+                f"{config.INDUCTION_URL}",
+                "Register for Induction",
+            )
+
         return Response()
 
 
