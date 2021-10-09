61fbe35e3708f40f3d9fa652d1ac59c49fe912fb
Jaimyn Mayer <github@jaimyn.com.au>
2021-10-09 16:35:58 +1000

    added generate xero invoice to membership invoice payment webhook

--- memberportal/api_billing/views.py
+++ memberportal/api_billing/views.py
@@ -1,6 +1,7 @@
 import python_http_client.exceptions
 
 from profile.models import Profile
+from profile.xerohelpers import create_stripe_membership_invoice
 from access.models import Doors, Interlock
 from api_admin_tools.models import *
 
@@ -557,7 +558,7 @@ class StripeWebhook(APIView):
             # If they aren't an active member, are NOT allowed to signup, and have paid the invoice
             # then we need to let them know and mark the subscription as active
             # (this could be a new OR returning member that's been too long since induction etc.)
-            if member.state != "active" and invoice_status == "paid":
+            elif member.state != "active" and invoice_status == "paid":
                 subject = "Your payment was successful."
                 title = subject
                 message = (
@@ -575,6 +576,28 @@ class StripeWebhook(APIView):
                 # already had this sent)
                 if member.state != "noob":
                     send_submitted_application_emails(member)
+
+            if member:
+                # check if the subscription is a membership subscription
+                subscription = MemberTier.objects.get(stripe_id=data["subscription"])
+
+                # return if the subscription is not a membership subscription
+                if not subscription:
+                    return Response()
+
+                # get the charge object so we can check how much the Stripe fee was
+                charge = stripe.Charge.retrieve(
+                    data["charge"], expand=["balance_transaction"]
+                )
+
+                # grab the amount and the Stripe fee
+                amount = charge.balance_transaction["amount"]
+                fee = charge.balance_transaction["fee"]
+
+                # generate a new Xero invoice
+                create_stripe_membership_invoice(
+                    user=member, amount=amount, fee_amount=fee
+                )
 
             # in all other instances, we don't care about a paid invoice and can ignore it
 
