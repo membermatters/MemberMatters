076386bd2dc3e381e4c9352a4f2f218a59f713df
Jaimyn Mayer <github@jaimyn.com.au>
2022-04-25 23:36:13 +1000

    added notification / route fade transitions back in

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -138,7 +138,7 @@ module.exports = configure((ctx) => ({
 
   // animations: 'all', // --- includes all animations
   // https://quasar.dev/options/animations
-  animations: [],
+  animations: ["fadeIn", "fadeOut"],
 
   // https://quasar.dev/quasar-cli/developing-ssr/configuring-ssr
   ssr: {
