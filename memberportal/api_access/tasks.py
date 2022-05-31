ec2196ba1c82d5e9a975130f3f1aeabb18f5e9f4
Jaimyn Mayer <github@jaimyn.com.au>
2022-05-31 23:04:40 +1000

    fixed fixtures

--- memberportal/api_access/tasks.py
+++ memberportal/api_access/tasks.py
@@ -5,7 +5,9 @@ from celery.schedules import crontab
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     # Calls test('world') every 30 seconds
-    sender.add_periodic_task(30.0, heartbeat_logger.s(), name="celery_heartbeat_logger")
+    sender.add_periodic_task(
+        30.0, heartbeat_logger.s(), expires=10, name="celery_heartbeat_logger"
+    )
 
 
 @app.task
