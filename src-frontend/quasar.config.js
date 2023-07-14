2dc9cc89f204506c8022d88037e18cb7b2beab99
Jaimyn Mayer <hello@jaimyn.com.au>
2023-07-14 16:15:08 +1000

    fixed up broken polyfills etc after move to vite

--- src-frontend/quasar.config.js
+++ src-frontend/quasar.config.js
@@ -10,18 +10,16 @@
 
 const { configure } = require('quasar/wrappers');
 const path = require('path');
-const NodeModulesPolyfillPlugin = require('@esbuild-plugins/node-modules-polyfill');
-const NodeGlobalsPolyfillPlugin = require('@esbuild-plugins/node-globals-polyfill');
-const rollupNodePolyFill = require('rollup-plugin-node-polyfills');
+
+const inject = require('@rollup/plugin-inject');
+const esbuildShim = require.resolve('node-stdlib-browser/helpers/esbuild/shim');
+
 const tsconfigPaths = require('vite-tsconfig-paths');
 
-module.exports = configure(function (ctx) {
+module.exports = configure(async function (ctx) {
+  const { default: stdLibBrowser } = await import('node-stdlib-browser');
   return {
     eslint: {
-      // fix: true,
-      // include: [],
-      // exclude: [],
-      // rawOptions: {},
       warnings: true,
       errors: true,
     },
@@ -50,7 +48,7 @@ module.exports = configure(function (ctx) {
         node: 'node18',
       },
 
-      htmlFilename: 'index.html',
+      htmlFilename: 'index2.html',
 
       env: {
         // When running with capacitor this value is used for the base URL for all API requests
@@ -69,52 +67,29 @@ module.exports = configure(function (ctx) {
       minify: true,
 
       extendViteConf(viteConf, { isServer, isClient }) {
-        viteConf.plugins.push({
-          name: 'fix-node-globals-polyfill',
-          setup(build) {
-            build.onResolve({ filter: /util\.js/ }, ({ path }) => ({ path }));
-          },
-        });
-
         viteConf.plugins.push(tsconfigPaths.default());
 
-        viteConf.build = {
-          ...viteConf.build,
-          build: {
-            rollupOptions: {
-              plugins: [
-                // Enable rollup polyfills plugin
-                // used during production bundling
-                rollupNodePolyFill(),
-              ],
-            },
-          },
-        };
+        viteConf.plugins.push({
+          ...inject({
+            global: [esbuildShim, 'global'],
+            process: [esbuildShim, 'process'],
+            Buffer: [esbuildShim, 'Buffer'],
+          }),
+          enforce: 'post',
+        });
 
         viteConf.optimizeDeps.esbuildOptions = {
           ...viteConf.optimizeDeps.esbuildOptions,
           define: {
             global: 'globalThis',
           },
-          plugins: [
-            NodeGlobalsPolyfillPlugin.NodeGlobalsPolyfillPlugin({
-              process: true,
-              buffer: true,
-            }),
-            NodeModulesPolyfillPlugin.NodeModulesPolyfillPlugin(),
-            {
-              name: 'fix-node-globals-polyfill',
-              setup(build) {
-                build.onResolve(
-                  { filter: /_virtual-process-polyfill_\.js/ },
-                  ({ path }) => ({ path })
-                );
-              },
-            },
-          ],
+          // Enable esbuild polyfill plugins
+          plugins: [],
         };
+
+        viteConf.optimizeDeps.include = ['buffer', 'process'];
       },
-      // viteVuePluginOptions: {},
+      viteVuePluginOptions: {},
 
       alias: {
         '@components': path.join(__dirname, 'src/components/'),
@@ -122,37 +97,7 @@ module.exports = configure(function (ctx) {
         '@store': path.join(__dirname, 'src/store/'),
         '@mixins': path.join(__dirname, 'src/mixins/'),
         '@assets': path.join(__dirname, 'src/assets/'),
-
-        util: 'util',
-        sys: 'util',
-        events: 'rollup-plugin-node-polyfills/polyfills/events',
-        stream: 'rollup-plugin-node-polyfills/polyfills/stream',
-        path: 'rollup-plugin-node-polyfills/polyfills/path',
-        querystring: 'rollup-plugin-node-polyfills/polyfills/qs',
-        punycode: 'rollup-plugin-node-polyfills/polyfills/punycode',
-        url: 'rollup-plugin-node-polyfills/polyfills/url',
-        string_decoder: 'rollup-plugin-node-polyfills/polyfills/string-decoder',
-        http: 'rollup-plugin-node-polyfills/polyfills/http',
-        https: 'rollup-plugin-node-polyfills/polyfills/http',
-        os: 'rollup-plugin-node-polyfills/polyfills/os',
-        assert: 'rollup-plugin-node-polyfills/polyfills/assert',
-        constants: 'rollup-plugin-node-polyfills/polyfills/constants',
-        _stream_duplex:
-          'rollup-plugin-node-polyfills/polyfills/readable-stream/duplex',
-        _stream_passthrough:
-          'rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough',
-        _stream_readable:
-          'rollup-plugin-node-polyfills/polyfills/readable-stream/readable',
-        _stream_writable:
-          'rollup-plugin-node-polyfills/polyfills/readable-stream/writable',
-        _stream_transform:
-          'rollup-plugin-node-polyfills/polyfills/readable-stream/transform',
-        timers: 'rollup-plugin-node-polyfills/polyfills/timers',
-        console: 'rollup-plugin-node-polyfills/polyfills/console',
-        vm: 'rollup-plugin-node-polyfills/polyfills/vm',
-        zlib: 'rollup-plugin-node-polyfills/polyfills/zlib',
-        tty: 'rollup-plugin-node-polyfills/polyfills/tty',
-        domain: 'rollup-plugin-node-polyfills/polyfills/domain',
+        ...stdLibBrowser,
       },
 
       vitePlugins: [
@@ -177,19 +122,19 @@ module.exports = configure(function (ctx) {
     devServer: {
       https: false,
       port: 8080,
-      open: true, // opens browser window automatically
+      open: false, // opens browser window automatically
       proxy: {
         // proxy requests when running dev server
         '/api': {
-          target: 'http://localhost:8000',
+          target: 'http://127.0.0.1:8000',
           changeOrigin: false,
         },
         '/admin': {
-          target: 'http://localhost:8000',
+          target: 'http://127.0.0.1:8000',
           changeOrigin: true,
         },
         '/static': {
-          target: 'http://localhost:8000',
+          target: 'http://127.0.0.1:8000',
           changeOrigin: true,
         },
       },
