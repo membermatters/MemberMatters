ec0e4675066fe6b2e99ef64b76a5c88c6f8a755d
Jaimyn Mayer <hello@jaimyn.com.au>
2023-07-29 21:31:25 +1000

    removed auto load fixtures from start script

--- docker/container_start.sh
+++ docker/container_start.sh
@@ -2,26 +2,21 @@
 
 if [ "$MM_RUN_MODE" = "celery_worker" ]
 then
-  echo celery worker mode
+  echo CONTAINER MODE: celery worker
   exec celery -A membermatters.celeryapp worker -l INFO
 elif [ "$MM_RUN_MODE" = "celery_beat" ]
 then
-  echo celery beat mode
+  echo CONTAINER MODE: celery beat
+  sleep 10 # wait a few seconds for other services to start
   exec celery -A membermatters.celeryapp beat -l INFO
 else
-  echo django mode
+  echo CONTAINER MODE: django webapp
+  sleep 5 # wait a few seconds for postgres to start
   # Start nginx
   nginx
 
   # We should migrate on startup in case there's been any db changes
   python3 manage.py migrate
 
-  if [ ! -f /usr/src/data/setupcomplete ]; then
-      echo "INFO: Detected a first time run. Populating the database with defaults."
-      python3 manage.py loaddata initial
-      touch /usr/src/data/setupcomplete
-  fi
-
-  #exec gunicorn membermatters.wsgi:application --bind unix:/tmp/gunicorn.sock --access-logfile '/usr/src/logs/access.log' --error-logfile '/usr/src/logs/error.log' --workers 6
   exec daphne -b 0.0.0.0 -p 8001 membermatters.asgi:application
 fi
