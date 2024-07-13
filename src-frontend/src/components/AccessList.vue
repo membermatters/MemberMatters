910aa197a5d38815415207a13a5c2bf389071cf3
Jaimyn Mayer <hello@jaimyn.dev>
2024-07-14 03:11:41 +1000

    Improved access permissions UI

--- src-frontend/src/components/AccessList.vue
+++ src-frontend/src/components/AccessList.vue
@@ -11,113 +11,116 @@
       </template>
       {{ $t('access.inactive') }}
     </q-banner>
-    <div class="row flex content-start items-start">
-      <q-list bordered padding class="rounded-borders q-ma-sm access-list">
-        <q-item-label header>
-          {{ $tc('access.door', 2) }}
-        </q-item-label>
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-md-6">
+        <q-list bordered padding class="rounded-borders">
+          <q-item-label header>
+            {{ $tc('access.door', 2) }}
+          </q-item-label>
 
-        <div class="q-pa-md" v-if="!doors.length">
-          {{ $t('doors.nodata') }}
-        </div>
+          <div class="q-pa-md" v-if="!doors.length">
+            {{ $t('doors.nodata') }}
+          </div>
 
-        <div
-          v-for="door in doors"
-          :key="door.name"
-          @click="
-            door.access
-              ? revoke('door', memberId, door.id)
-              : authorise('door', memberId, door.id)
-          "
-        >
-          <q-item>
-            <q-item-section avatar top>
-              <q-avatar
-                :icon="icons.doorOpen"
-                :color="
-                  door.locked_out ? 'orange' : door.access ? 'green' : 'red'
-                "
-                text-color="white"
-              />
-            </q-item-section>
+          <div
+            v-for="door in doors"
+            :key="door.name"
+            @click="
+              door.access
+                ? revoke('door', memberId, door.id)
+                : authorise('door', memberId, door.id)
+            "
+          >
+            <q-item>
+              <q-item-section avatar top>
+                <q-avatar
+                  :icon="icons.doorOpen"
+                  :color="
+                    door.locked_out ? 'orange' : door.access ? 'green' : 'red'
+                  "
+                  text-color="white"
+                />
+              </q-item-section>
 
-            <q-item-section>
-              <q-item-label lines="1">
-                {{ door.name }}
-              </q-item-label>
-              <q-item-label v-if="door.locked_out === true" caption>
-                <div>{{ $t('access.maintenance') }}</div>
-                <div v-if="door.access === true">
-                  ({{ $t('access.authorised') }})
-                </div>
-                <div v-else>({{ $t('access.unauthorised') }})</div>
-              </q-item-label>
-              <q-item-label v-else-if="door.access === true" caption>
-                {{ $t('access.authorised') }}
-              </q-item-label>
-              <q-item-label v-else caption>
-                {{ $t('access.unauthorised') }}
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-        </div>
-      </q-list>
+              <q-item-section>
+                <q-item-label lines="1">
+                  {{ door.name }}
+                </q-item-label>
+                <q-item-label v-if="door.locked_out === true" caption>
+                  <div>{{ $t('access.maintenance') }}</div>
+                  <div v-if="door.access === true">
+                    ({{ $t('access.authorised') }})
+                  </div>
+                  <div v-else>({{ $t('access.unauthorised') }})</div>
+                </q-item-label>
+                <q-item-label v-else-if="door.access === true" caption>
+                  {{ $t('access.authorised') }}
+                </q-item-label>
+                <q-item-label v-else caption>
+                  {{ $t('access.unauthorised') }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </div>
+        </q-list>
+      </div>
 
-      <q-list bordered padding class="rounded-borders q-ma-sm access-list">
-        <q-item-label header>
-          {{ $tc('access.interlock', 2) }}
-        </q-item-label>
+      <div class="col-12 col-md-6">
+        <q-list bordered padding class="col-12 col-md-6 rounded-borders">
+          <q-item-label header>
+            {{ $tc('access.interlock', 2) }}
+          </q-item-label>
 
-        <q-item-label class="q-pa-md" v-if="!interlocks.length">
-          {{ $t('interlocks.nodata') }}
-        </q-item-label>
+          <q-item-label class="q-pa-md" v-if="!interlocks.length">
+            {{ $t('interlocks.nodata') }}
+          </q-item-label>
 
-        <div
-          v-for="interlock in interlocks"
-          :key="interlock.name"
-          @click="
-            interlock.access
-              ? revoke('interlock', memberId, interlock.id)
-              : authorise('interlock', memberId, interlock.id)
-          "
-        >
-          <q-item>
-            <q-item-section avatar top>
-              <q-avatar
-                :icon="icons.interlock"
-                :color="
-                  interlock.locked_out
-                    ? 'orange'
-                    : interlock.access
-                    ? 'green'
-                    : 'red'
-                "
-                text-color="white"
-              />
-            </q-item-section>
+          <div
+            v-for="interlock in interlocks"
+            :key="interlock.name"
+            @click="
+              interlock.access
+                ? revoke('interlock', memberId, interlock.id)
+                : authorise('interlock', memberId, interlock.id)
+            "
+          >
+            <q-item>
+              <q-item-section avatar top>
+                <q-avatar
+                  :icon="icons.interlock"
+                  :color="
+                    interlock.locked_out
+                      ? 'orange'
+                      : interlock.access
+                      ? 'green'
+                      : 'red'
+                  "
+                  text-color="white"
+                />
+              </q-item-section>
 
-            <q-item-section>
-              <q-item-label lines="1">
-                {{ interlock.name }}
-              </q-item-label>
-              <q-item-label v-if="interlock.locked_out === true" caption>
-                {{ $t('access.maintenance') }}
-                <div v-if="door.access === true">
-                  ({{ $t('access.authorised') }})
-                </div>
-                <div v-else>({{ $t('access.unauthorised') }})</div>
-              </q-item-label>
-              <q-item-label v-else-if="interlock.access === true" caption>
-                {{ $t('access.authorised') }}
-              </q-item-label>
-              <q-item-label v-else caption>
-                {{ $t('access.unauthorised') }}
-              </q-item-label>
-            </q-item-section>
-          </q-item>
-        </div>
-      </q-list>
-
+              <q-item-section>
+                <q-item-label lines="1">
+                  {{ interlock.name }}
+                </q-item-label>
+                <q-item-label v-if="interlock.locked_out === true" caption>
+                  {{ $t('access.maintenance') }}
+                  <div v-if="door.access === true">
+                    ({{ $t('access.authorised') }})
+                  </div>
+                  <div v-else>({{ $t('access.unauthorised') }})</div>
+                </q-item-label>
+                <q-item-label v-else-if="interlock.access === true" caption>
+                  {{ $t('access.authorised') }}
+                </q-item-label>
+                <q-item-label v-else caption>
+                  {{ $t('access.unauthorised') }}
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </div>
+        </q-list>
+      </div>
       <refresh-data-dialog v-model="errorLoading" />
     </div>
   </div>
@@ -230,9 +233,3 @@ export default {
   },
 };
 </script>
-
-<style lang="sass" scoped>
-@media (max-width: $breakpoint-xs-max)
-  .access-list
-    width: 100%
-</style>
